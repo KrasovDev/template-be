dc033e7a56b0cc44a74241bf42b11f9686b0a8d8
Dmytro Vidieiev <dimich2993@gmail.com>
2022-03-27 01:39:51 +0200

    feat: add swagger module

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,29 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { DBService } from './db.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({}),
   );
-  await app.listen(3000);
 
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  // Bootstrap swagger documentation
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Attach app close events to correctly close db connection
   const prismaService = app.get(DBService);
   await prismaService.enableShutdownHooks(app);
+
+  await app.listen(3000);
 }
 bootstrap();
